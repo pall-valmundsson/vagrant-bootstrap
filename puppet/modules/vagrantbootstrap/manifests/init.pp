724cd0b7fffcdc0fb4fa2147b78f131e23575467
Pall Valmundsson <pall.valmundsson@gmail.com>
2013-09-18 00:05:05 +0000

    fix runlibrarian helper

--- puppet/modules/vagrantbootstrap/manifests/init.pp
+++ puppet/modules/vagrantbootstrap/manifests/init.pp
@@ -9,13 +9,13 @@ class vagrantbootstrap::repos {
     $epelrpm = 'http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm'
 
     exec { "yum -y install $epelrpm":
-        path => '/usr/bin',
+        path    => '/usr/bin',
         creates => '/etc/yum.repos.d/epel.repo',
     }
 
     #exec { 'yum -y update && touch /tmp/yum-updated':
     #    timeout => 0,
-    #    unless => 'test -e /tmp/yum-updated',
+    #    unless  => 'test -e /tmp/yum-updated',
     #}
 }
 
@@ -31,12 +31,12 @@ class vagrantbootstrap::helpers {
     $puppet_dir = '/vagrant/puppet'
 
     file { '/usr/local/bin/runpuppet':
-        content => "sudo puppet apply -vv  --modulepath=$puppet_dir/modules/ $puppet_dir/manifests/main.pp\n",
-        mode => '0755',
+        content => "sudo puppet apply -vv  --modulepath=${puppet_dir}/modules/ $puppet_dir/manifests/main.pp\n",
+        mode    => '0755',
     }
 
     file { '/usr/local/bin/runlibrarian':
-        content => 'cd $puppet_dir &&  sudo librarian-puppet update \n',
-        mode => '0755',
+        content => "cd ${puppet_dir} &&  sudo librarian-puppet update \n",
+        mode    => '0755',
     }
 }
